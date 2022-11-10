97e69ebe8c72904d4c1f76c807a0d8dd89386d58
Mohammad Rasuli <113033874+rasuli22@users.noreply.github.com>
2022-11-10 12:07:02 +0000

    Update application.py

--- application.py
+++ application.py
@@ -67,7 +67,7 @@ welcome = """
 
     text-align: right;
     padding-top: 11em;
-    background-color: #Magenta;
+    background-color: #Orange;
     background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
     background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
   }
